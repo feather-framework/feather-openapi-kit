99b44e3e71af3b99a570c4e25fbbcfde98b261e4
Tibor Bodecs <mail.tib@gmail.com>
2024-01-25 23:45:23 +0100

    add standard response types

--- Sources/FeatherOpenAPIKit/Interfaces/Operation/OperationResponse.swift
+++ Sources/FeatherOpenAPIKit/Interfaces/Operation/OperationResponse.swift
@@ -1,12 +1,12 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Tibor Bodecs on 25/01/2024.
 //
 
 public struct OperationResponse {
-
+    
     public let statusCode: Int
     public let response: Response.Type
     
@@ -18,3 +18,51 @@ public struct OperationResponse {
         self.response = response
     }
 }
+
+public extension OperationResponse {
+    
+    static func ok(_ response: Response.Type) -> Self {
+        .init(200, response)
+    }
+    
+    static func badRequest(_ response: Response.Type) -> Self {
+        .init(400, response)
+    }
+    
+    static func unauthorized(_ response: Response.Type) -> Self {
+        .init(401, response)
+    }
+    
+    static func forbidden(_ response: Response.Type) -> Self {
+        .init(403, response)
+    }
+    
+    static func notFound(_ response: Response.Type) -> Self {
+        .init(404, response)
+    }
+    
+    static func methodNotAllowed(_ response: Response.Type) -> Self {
+        .init(405, response)
+    }
+    
+    static func notAcceptable(_ response: Response.Type) -> Self {
+        .init(406, response)
+    }
+    
+    static func conflict(_ response: Response.Type) -> Self {
+        .init(409, response)
+    }
+    
+    static func gone(_ response: Response.Type) -> Self {
+        .init(410, response)
+    }
+    
+    static func unsupportedMediaType(_ response: Response.Type) -> Self {
+        .init(415, response)
+    }
+    
+    static func unprocessableContent(_ response: Response.Type) -> Self {
+        .init(422, response)
+    }
+}
+
