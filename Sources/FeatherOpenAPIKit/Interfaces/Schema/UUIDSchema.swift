716cb09646016d5fcc29fef8e7bc9c16a079f855
László Kovács <atomantinator@gmail.com>
2024-03-28 08:15:56 +0100

    remove randomness from examples

--- Sources/FeatherOpenAPIKit/Interfaces/Schema/UUIDSchema.swift
+++ Sources/FeatherOpenAPIKit/Interfaces/Schema/UUIDSchema.swift
@@ -15,7 +15,11 @@ public protocol UUIDSchema: Schema {
 public extension UUIDSchema {
 
     static var examples: [UUID] {
-        (0..<5).map { _ in .init() }
+        [.init(uuidString: "F257448D-73F6-4D6F-BB8A-8D756A622F70")!,
+         .init(uuidString: "84FB7C06-838A-4712-9E2A-C76294670C04")!,
+         .init(uuidString: "BA4D8D41-BF9F-4122-8546-AB82A98BE28F")!,
+         .init(uuidString: "519749BB-1210-4479-9B9E-292FC76444C7")!,
+         .init(uuidString: "E0A42968-28EE-4D1F-BE18-13D526458686")!]
     }
 
     static func openAPISchema() -> JSONSchema {
